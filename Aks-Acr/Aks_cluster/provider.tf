5307c8f98f7f9708e457e02760259eea0adc988b
Alok Vaishya <alokp0913@gmail.com>
2025-09-02 11:43:02 +0530

    prvider file added

---
+++ Aks-Acr/Aks_cluster/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.42.0"
+    }
+  }
+  backend "azurerm" {
+    storage_account_name = "actionstorage0033"
+    container_name = "actionblob001"
+    key = "action.tfstate"
+    
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {
+    
+  }
+  subscription_id = "75330da7-9d4b-4fbe-bb6f-8c4125ece9a6"
+}
