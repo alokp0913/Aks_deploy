51a108d3fe7d40288074b546a556e3df1f03df5f
alokp0913 <123491037+alokp0913@users.noreply.github.com>
2025-09-08 02:02:21 +0530

    Update azurerm_aks_cluster.tf

--- Aks-Acr/Aks_cluster/azurerm_aks_cluster.tf
+++ Aks-Acr/Aks_cluster/azurerm_aks_cluster.tf
@@ -14,7 +14,7 @@ depends_on = [azurerm_resource_group.example]
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_D2_v2"
+    vm_size    = "standard_a2_v2"
   }
 
   identity {
