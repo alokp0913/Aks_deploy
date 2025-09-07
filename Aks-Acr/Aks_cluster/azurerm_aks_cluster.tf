915fa0f7c3357421cccc6f668d0c58826bd64375
alokp0913 <123491037+alokp0913@users.noreply.github.com>
2025-09-08 02:06:40 +0530

    Update azurerm_aks_cluster.tf

--- Aks-Acr/Aks_cluster/azurerm_aks_cluster.tf
+++ Aks-Acr/Aks_cluster/azurerm_aks_cluster.tf
@@ -3,12 +3,18 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
-resource "azurerm_kubernetes_cluster" "example" {
-depends_on = [azurerm_resource_group.example]
+
+resource "azurerm_resource_group" "example1" {
+  name     = tinku00223"
+  location = "Korea Southe"
+}
+
+resource "azurerm_kubernetes_cluster" "example1" {
+depends_on = [azurerm_resource_group.example1]
 
   name                = "akspinkutinku00223"
-  location            = "West Europe"
-  resource_group_name = "pinkutinku00223"
+  location            = "Korea Southe"
+  resource_group_name = "tinku00223"
   dns_prefix          = "exampleaks1"
 
   default_node_pool {
